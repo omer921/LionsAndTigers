232ee9d8218c6e3d85c886a6fcbb15e4f7714a1e
Omer921 <Omer921@gmail.com>
2015-07-02 09:46:33 +0700

    random fact added

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,62 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Omer Winrauke on 6/30/15.
+//  Copyright (c) 2015 Omer Winrauke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0;
+    var name = "";
+    var breed = "";
+    var image = UIImage(named:"");
+    
+    func chuff() {
+        println("Tiger: chuff chuff");
+    }
+    
+    func chuffANumberOfTimes(numberOfTimes:Int) {
+        for (var chuff = 0; chuff < numberOfTimes; chuff++) {
+            self.chuff();
+        }
+    }
+    
+    func chuffANumberOfTimes(numberOfTimes:Int, isLoud:Bool) {
+        for (var chuffTimes = 0; chuffTimes < numberOfTimes; chuffTimes++) {
+            if (isLoud) {
+                chuff();
+            }
+            else {
+                println("Tiger: purr purr");
+            }
+        }
+    }
+    
+    func ageInTigerYearsFromAge(regularAge:Int) -> Int {
+        return (regularAge * 3);
+    }
+    
+    
+    func randomFact() -> String {
+        let randomNumber = Int(arc4random_uniform(UInt32(3)));
+        
+        var randomFact:String;
+        
+        if (randomNumber == 0) {
+            randomFact = "The tiger is the biggest species in the cat family";
+        }
+        else if (randomNumber == 1) {
+            randomFact = "Tigers can reach a length of 3.3 meters";
+        }
+        else {
+            randomFact = "Group of tigers is known as an ambush or streak";
+        }
+        return randomFact;
+    }
+    
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -9,15 +9,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var randomFactLabel: UILabel!
+    
+    var currentIndex = 0;
+    
+    var myTigers:[Tiger] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger();
+        myTiger.name = "Tigger";
+        myTiger.breed = "Bengal";
+        myTiger.age = 3;
+        myTiger.image = UIImage(named: "BengalTiger.jpg");
+        myTiger.chuff();
+        myTiger.chuffANumberOfTimes(5, isLoud: false);
+        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age);
+        
+        myTigers.append(myTiger);
+        
+        myImageView.image = myTiger.image;
+        nameLabel.text = myTiger.name;
+        ageLabel.text = "\(myTiger.age)";
+        breedLabel.text = myTiger.breed;
+        randomFactLabel.text = myTiger.randomFact();
+        
+        
+        var secondTiger = Tiger();
+        secondTiger.name = "Tigress";
+        secondTiger.breed = "Indochinese Tiger";
+        secondTiger.age = 2;
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg");
+        
+        
+        
+        var thirdTiger = Tiger();
+        thirdTiger.name = "Jacob";
+        thirdTiger.breed = "Malayin Tiger";
+        thirdTiger.age = 4;
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg");
+        
+        var fourthTiger = Tiger();
+        fourthTiger.name = "Spar";
+        fourthTiger.breed = "Siberian Tiger";
+        fourthTiger.age = 5;
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg");
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger];
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int;
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)));
+        } while (randomIndex == currentIndex);
+        let tiger = myTigers[randomIndex];
+        
+        currentIndex = randomIndex;
+        
+        
+//        myImageView.image = tiger.image;
+//        nameLabel.text = tiger.name;
+//        ageLabel.text = "\(tiger.age)";
+//        breedLabel.text = tiger.breed;
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            self.myImageView.image = tiger.image;
+            self.nameLabel.text = tiger.name;
+            self.ageLabel.text = "\(tiger.age)";
+            self.breedLabel.text = tiger.breed;
+            self.randomFactLabel.text = tiger.randomFact();
+            }, completion: { (finished: Bool) -> () in});
+        
+        
     }
 
 
